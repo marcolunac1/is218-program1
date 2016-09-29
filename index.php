e7ed4f40b7f6129bc6d50e5f79bff46f9604450f
Marco Luna <marcolunac1@gmail.com>
2016-09-29 14:24:10 -0400

    added objects

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	//This is an example of php arrays
 	
 	$myarray = array();
+	//This is how you add values to an array
 	$myarray [] = 'some value 1';
 	$myarray [] = 'some value 2';
 	$myarray [] = 'some value 3';
@@ -22,6 +23,35 @@
 	$myAssoc = array('value1' => $myarray, 'value2' => $myarray);
 
 	print_r($myAssoc);
-	
+	//This is how you print_r / access to a array by naming the key you want to access
+	print_r($myAssoc['value1']);
+
+	//This is how you define a class in php
+	class myclass {
+	  public $myPublic;
+	  private $myPrivate;
+	  protected $myProtected;
+
+	  public function __construct() {
+	    //This is the internal reference to the object within a class, the arroy points
+	    //to a property or method being accessed within the object
+	    $this->myPublic = 1;
+ 	    $this->myPrivate = 2; 
+	    $this->myProtected = 3;
+	    //This is how you  call a method 
+	    $this->sayHello('Marco');
+	}
+
+	public function sayHello($name) {
+
+	  echo 'Hello' . $name; 
+	}
+
+}
+	//This is how you instantiate a new class object
+	$obj = new myclass;
+
+	print_r($obj);
+
 
 ?>
