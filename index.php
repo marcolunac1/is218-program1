5485535f31bd2170226a7ffb5f001d25178b2631
Marco Luna <marcolunac1@gmail.com>
2016-09-29 13:50:42 -0400

    added some arrays

--- index.php
+++ index.php
@@ -4,10 +4,24 @@
 	//This is how you store a value in a variable
 	$myvar = 'Some text I want to store in a variable';
 
-	//THis is an example of the difference between single quotes and double quotes
+	//This is an example of the difference between single quotes and double quotes
 	echo '<br>';
 	echo '$myvar';
 	echo '<br>';
 	echo "$myvar";
+
+	//This is an example of php arrays
+	
+	$myarray = array();
+	$myarray [] = 'some value 1';
+	$myarray [] = 'some value 2';
+	$myarray [] = 'some value 3';
+
+	print_r($myarray);
+	//THis is an example of an associative array and a nested array
+	$myAssoc = array('value1' => $myarray, 'value2' => $myarray);
+
+	print_r($myAssoc);
+	
 
 ?>
