7f2853093a454619866c6906a590c3206ffdc022
Marco Luna <marcolunac1@gmail.com>
2016-09-29 13:35:44 -0400

    added some code to the program

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+	//This is how you print text in php
+	echo 'hello world'; 
+	//This is how you store a value in a variable
+	$myvar = 'Some text I want to store in a variable';
 
-echo "hello world"; 
+	//THis is an example of the difference between single quotes and double quotes
+	echo '<br>';
+	echo '$myvar';
+	echo '<br>';
+	echo "$myvar";
 
 ?>
-
